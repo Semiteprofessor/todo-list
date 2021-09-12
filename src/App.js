f9434a6dc1040e666dc9d594702168bfa3ec18a8
semiteprofessor <semiteprofessor@gmail.com>
2021-09-12 10:40:03 +0100

    edited addForm file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import Man from './man'
+import AddForm from './components/addform'
 
 export default class App extends Component {
 
   state = {
     manchester: [
-      {name: "Cristiano Ronaldo", age: 36, id: 1},
-      {name: "Edinson Cavani", age: 34, id: 2},
-      {name: "Jadon Sancho", age: 21, id: 3},
-      {name: "Mason Greenwood", age: 19, id: 4},
-      {name: "Paul Pogba", age: 28, id: 5},
-      {name: "Bruno Fernandes", age: 27, id: 6}
+      {name: "Cristiano Ronaldo", age: 36, position: "Winger", id: 1},
+      {name: "Edinson Cavani", age: 34, position: "Striker", id: 2},
+      {name: "Jadon Sancho", age: 21, position: "Winger", id: 3},
+      {name: "Mason Greenwood", age: 19, position: "Winger", id: 4},
+      {name: "Paul Pogba", age: 28, position: "Midfielder", id: 5},
+      {name: "Bruno Fernandes", age: 27, position: "Midfielder", id: 6},
+      {name: "Rafael Varane", age: 28, position: "Defender", id: 6},
+      {name: "David De Gea", age: 31, position: "Goalkeeper", id: 6}
     ]
   }
 
@@ -29,6 +32,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <Man manchester={this.state.manchester} />
+        <AddForm />
       </div>
     )
   }
